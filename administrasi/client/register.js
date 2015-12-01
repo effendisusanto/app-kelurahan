9136408129d8288222183632072e909d7eaf44f3
trihatmaja <trihatmaja@gmail.com>
2015-12-01 20:29:36 +0700

    fixing komentar, register

--- administrasi/client/register.js
+++ administrasi/client/register.js
@@ -3,9 +3,17 @@ Template.register.onRendered(function() {
     this.$('.selectpicker').selectpicker();
 });
 
-
 Template.register.events({
-    'submit form': function(event){
+    'change input[type="file"]': function(event, template) {
+        var reader = new FileReader();
+        var file = template.find('#profilePicture').files[0]; // get the file
+        reader.onloadend = function(event) {
+            template.find('#preview').src = event.target.result;
+            Session.set("dataUrl", event.target.result);
+        }
+        reader.readAsDataURL(file);
+    },
+    'submit form': function(event, template) {
         event.preventDefault();
         var email = $('#email').val();
         var password = $('#inputPassword').val();
@@ -15,24 +23,26 @@ Template.register.events({
         var pangkat = $("#pangkat").val();
         var nip = $("#nip").val();
         var groupLogin = $("#loginGroup").val();
+        var dataUrl = Session.get("dataUrl");
         Accounts.createUser({
-            email: email,
-            password: password,
-            profile:{
-                namaDepan: namaDepan,
-                namaBelakang: namaBelakang,
-                gelar: gelar,
-                pangkat: pangkat,
-                nip: nip,
-                groupLogin: groupLogin
+                email: email,
+                password: password,
+                profile: {
+                    namaDepan: namaDepan,
+                    namaBelakang: namaBelakang,
+                    gelar: gelar,
+                    pangkat: pangkat,
+                    nip: nip,
+                    groupLogin: groupLogin,
+                    picture: dataUrl
                 }
-            }, 
-            function(error){
-            if(error){
-                console.log(error.reason); // Output error if registration fails
-            } else {
-                Router.go("/"); // Redirect user if registration succeeds
-            }
-        });
+            },
+            function(error) {
+                if (error) {
+                    console.log(error.reason); // Output error if registration fails
+                } else {
+                    Router.go("/"); // Redirect user if registration succeeds
+                }
+            });
     }
 });
--- suratketerangan/client/appsuket_komentar.js
+++ suratketerangan/client/appsuket_komentar.js
@@ -1,61 +1,73 @@
 Meteor.subscribe("main")
 Meteor.subscribe("komentar")
 
-Template.komentar.onRendered(function(){
-	this.$('#formKomentar').validator();
+Template.komentar.onRendered(function() {
+    this.$('#formKomentar').validator();
 });
 
 Template.komentar.helpers({
-	listKomentar: function(){
-        return Komentar.find({mainId:this.mainId});
-	},
-	penulis: function(){
-		return Meteor.users.findOne(this.userId);
-	}
+    listKomentar: function() {
+        return Komentar.find({
+            mainId: this.mainId
+        });
+    },
+    penulis: function() {
+        return Meteor.users.findOne(this.userId);
+    }
 });
 
 Template.komentar.events({
-	'click #approve': function(){
-		var date = new Date();
-		var id = this.mainId;
-		var komentar = $("#komentar").val();
-		var statusSkr = Main.findOne({_id:this.mainId}, {"status":1, _id:0});
-		var status = ""
-		switch(statusSkr.status){
-			case "Staff Kelurahan":
-				status = "Sekertaris Lurah";
-				break;
-			case "Sekertaris Lurah":
-				status = "Lurah"
-				break;
-			case "Lurah":
-				status = "Cetak"
-				break;
-		}
-		// TODO: add user
-		Meteor.call("approval", id, moment(date).format("dddd, D MMMM YYYY, h:mm"), status, komentar, Meteor.userId());
-		$("#komentar").val("");
-		$('#page-wrapper').html("");
-		return UI.insert(UI.render(Template.tasktodo), $('#page-wrapper').get(0));
-	},
-	'click #reject': function(){
-		var date = new Date();
-		var id = this.mainId;
-		var komentar = $("#komentar").val();
-		var statusSkr = Main.findOne({_id:this.mainId}, {"status":1, _id:0});
-		var status = ""
-		switch(statusSkr.status){
-			case "Lurah":
-				status = "Sekertaris Lurah";
-				break;
-			case "Sekertaris Lurah":
-				status = "Staff Kelurahan"
-				break;
-		}
-		// TODO: add user
-		Meteor.call("approval", id, moment(date).format("dddd, D MMMM YYYY, h:mm"), status, komentar, Meteor.userId());
-		$("#komentar").val();
-		$('#page-wrapper').html("");
-		return UI.insert(UI.render(Template.tasktodo), $('#page-wrapper').get(0));
-	}
+    'click #approve': function() {
+        var date = new Date();
+        var id = this.mainId;
+        var komentar = $("#komentar").val();
+        var statusSkr = Main.findOne({
+            _id: this.mainId
+        }, {
+            "status": 1,
+            _id: 0
+        });
+        var status = ""
+        switch (statusSkr.status) {
+            case "Staff Kelurahan":
+                status = "Sekertaris Lurah";
+                break;
+            case "Sekertaris Lurah":
+                status = "Lurah"
+                break;
+            case "Lurah":
+                status = "Cetak"
+                break;
+        }
+        // TODO: add user
+        Meteor.call("approval", id, moment(date).format("dddd, D MMMM YYYY, h:mm"), status, komentar, Meteor.userId());
+        $("#komentar").val("");
+        $('#page-wrapper').html("");
+        return UI.insert(UI.render(Template.tasktodo), $('#page-wrapper').get(0));
+    },
+    'click #reject': function() {
+        var date = new Date();
+        var id = this.mainId;
+        var komentar = $("#komentar").val();
+        var statusSkr = Main.findOne({
+            _id: this.mainId
+        }, {
+            "status": 1,
+            _id: 0
+        });
+        var status = ""
+        switch (statusSkr.status) {
+            case "Lurah":
+                status = "Sekertaris Lurah";
+                break;
+            case "Sekertaris Lurah":
+                status = "Staff Kelurahan"
+                break;
+        }
+        // TODO: add user
+        Meteor.call("approval", id, moment(date).format("dddd, D MMMM YYYY, h:mm"), status, komentar, Meteor.userId());
+        $("#komentar").val();
+        $('#page-wrapper').html("");
+        return UI.insert(UI.render(Template.tasktodo), $('#page-wrapper').get(0));
+    }
 });
